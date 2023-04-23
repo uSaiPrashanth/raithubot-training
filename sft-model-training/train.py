aeaf249027b2006f9cd845e960b6f8c5948bbb99
USVSN Sai Prashanth <usaiprashanth2018@gmail.com>
2023-04-23 18:54:07 +0000

    Allow Gradient accumulation

--- sft-model-training/train.py
+++ sft-model-training/train.py
@@ -6,48 +6,74 @@ from functools import partial
 import oslo
 import os
 from tqdm.auto import tqdm
+import numpy as np
 import jsonlines
 import torch
 
-
 def train_model(parallel_context):
-    model = AutoModelForCausalLM.from_pretrained('EleutherAI/pythia-12b', cache_dir = './hf-models')
+    model = AutoModelForCausalLM.from_pretrained('EleutherAI/gpt-neox-20b', cache_dir = './hf-models')
     tokenizer = AutoTokenizer.from_pretrained(
-        'EleutherAI/pythia-12b', 
+        'EleutherAI/gpt-neox-20b', 
         cache_dir = './hf-models',
+        additional_special_tokens = (
+            '<|query|>',
+            '</query/>',
+            '<|response|>',
+            '</response/>',
+            '<|result|>',
+            '</result/>',
+            '<|summary|>',
+            '</summary/>',
+            '<|results|>',
+            '</results/>',
+            '<|result-sentence|>'
+        )
     )
 
-    with open("kcc_data/jsonlines/data.jsonl", 'r') as reader:
+    with open("kcc_data/jsonlines/trimmed.jsonl", 'r') as reader:
         data = [i[1:-2] for i in reader]
 
     model = TensorParallel(model, parallel_context)
     oslo.ready(model, parallel_context)
 
+    scaler = torch.cuda.amp.GradScaler()
     optimizer = torch.optim.Adam(model.parameters(), lr = 1e-6)
 
-    with tqdm(data) as pbar:
-        for record in pbar:
-            tokens = tokenizer(
-                [record], 
-                return_tensors = 'pt',
-                truncation = True,
-                max_length = 2048
-            )
-            input_ids = tokens['input_ids'][:, 0:-1].cuda()
-            labels = tokens['input_ids'][:, 1:].cuda()
-    
-            optimizer.zero_grad()
-            loss = model(input_ids = input_ids, labels = labels).loss
-            loss.backward()
-            optimizer.step()
+    gradient_accumilation_steps = 8
+    for epoch in range(20):
+        np.random.shuffle(data)
+        with tqdm(data) as pbar:
+            step = 0
+            for record in pbar:
+                optimizer.zero_grad()
+                tokens = tokenizer(
+                    [record], 
+                    return_tensors = 'pt',
+                    truncation = True,
+                    max_length = 1536
+                )
+                input_ids = tokens['input_ids'][:, 0:-1].cuda()
+                labels = tokens['input_ids'][:, 1:].cuda()
 
-            pbar.set_description(f"LOSS: {loss.item():.3f}")
-        
-        
+                with torch.cuda.amp.autocast():
+                    loss = model.forward(input_ids = input_ids, labels = labels).loss
+                
+                scaler.scale(loss).backward()
+
+                if (step + 1) % gradient_accumilation_steps == 0:
+                    scaler.step(optimizer)
+
+                    scaler.update()
+                    pbar.set_description(f"LOSS: {loss.item():.3f}")
+
+                step += 1
+
     model.save_pretrained(save_directory = "./hf-models/usvsnsp-sft-model/")
+    tokenizer.save_pretrained(save_directory = "./hf-models/usvsnsp-sft-model/")
+    tokenizer.save_pretrained(save_directory = "./hf-models/usvsnsp-sft-combined/")
 
 def deparallelize_save(parallel_context):
-    model = AutoModelForCausalLM.from_pretrained('EleutherAI/pythia-12b', cache_dir = './hf-models')
+    model = AutoModelForCausalLM.from_pretrained('EleutherAI/gpt-neox-20b', cache_dir = './hf-models')
     model = TensorParallel(model, parallel_context)
     oslo.ready(model, parallel_context)
 
@@ -64,5 +90,6 @@ if __name__ == '__main__':
         tensor_parallel_size=int(os.environ['WORLD_SIZE']),
         tensor_parallel_mode=ParallelMode.TENSOR_1D,
     )
-    # train_model(parallel_context)
+    train_model(parallel_context)
+    torch.cuda.empty_cache()
     deparallelize_save(parallel_context)
